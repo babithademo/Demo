922a8d319376c044f0953958e67a5bcf16d69970
“babitha” <babithadavis5@gmail.com>
2022-07-24 20:42:31 +0530

    committed

--- gitpush.sh
+++ gitpush.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 
 git add .
-read -p "Enter commit message: " commit_message
-git commit -m "$commit_message"
-BRANCH=$(git describe --contains --all HEAD)
-git pull --rebase origin "$BRANCH"
-git push origin "$BRANCH"
+
+echo 'Enter the commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+echo 'Enter the name of the branch:'
+read branch
+
+git push origin $branch
+
+read
